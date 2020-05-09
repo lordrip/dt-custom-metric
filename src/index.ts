6646b61c8e9688f78e709b32d5bd6894780409e1
Ricaro M <lordrip@gmail.com>
2020-05-09 10:05:24 +0200

    adjust ticker to 30s

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { interval } from 'rxjs';
-import { switchMap, take, map, mapTo } from 'rxjs/operators';
+import { concatMap, map, mapTo } from 'rxjs/operators';
 import { publishMetric } from './load-generation';
 
-interval(2000)
+interval(30_000)
   .pipe(
-    take(50),
     mapTo(Math.random() * 1_000),
-    switchMap((value) => publishMetric(value)),
+    concatMap((value) => publishMetric(value)),
     map((response) => ({ status: response.status, text: response.statusText })),
   )
   .subscribe(console.log)
