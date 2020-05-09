0d384ecd3b593eabc1015019966d028e7c9e237b
Ricaro M <lordrip@gmail.com>
2020-05-09 10:15:07 +0200

    Add generated value to log

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { interval } from 'rxjs';
-import { concatMap, map, mapTo } from 'rxjs/operators';
+import { from, interval } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
 import { publishMetric } from './load-generation';
 
 interval(30_000)
   .pipe(
-    mapTo(Math.random() * 1_000),
-    concatMap((value) => publishMetric(value)),
-    map((response) => ({ status: response.status, text: response.statusText })),
+    map(() => Math.random() * 1_000),
+    concatMap(
+      (value) => from(
+        publishMetric(value)
+      )
+        .pipe(
+          map((response) => ({ response, value }))
+        ),
+    ),
+    map(({ response: { status, statusText }, value }, index) => ({ index, status, statusText, value })),
   )
-  .subscribe(console.log)
+    .subscribe({
+      next: (value) => {
+        const date = new Date();
+
+        console.log(`${date.toLocaleString()} - ${value.value} - Beacon #: ${value.index} | status: ${value.status}:${value.statusText}`)
+      }
+    });
